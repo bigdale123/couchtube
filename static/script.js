51dd39f7525b0a294dfcd0e3eac0b7de6049a7bb
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2024-10-29 15:21:30 +0300

    Wip - buffering state

--- static/script.js
+++ static/script.js
@@ -1,13 +1,18 @@
 const IFRAME_API_URL = 'https://www.youtube.com/iframe_api';
 const BUFFERING_TIMEOUT = 2000;
+const CHANNELS_ENDPOINT = '/channels';
 
 class YouTubePlayer {
-  channelsUrl = '/channels';
-
   constructor(playerElementId) {
     this.player = null;
     this.playerReady = false;
     this.playerElementId = playerElementId;
+    this.channelsUrl = CHANNELS_ENDPOINT;
+    this.channels = [];
+    this.currentChannel = null;
+    this.videoTitle = '';
+    this.isPlaying = false;
+    this.isBuffering = true;
 
     this.loadYouTubeAPI();
     this.loadChannels();
@@ -20,7 +25,6 @@ class YouTubePlayer {
     const firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    // global callback
     window.onYouTubeIframeAPIReady = () => this.onYouTubeIframeAPIReady();
   }
 
@@ -28,6 +32,7 @@ class YouTubePlayer {
     this.player = new YT.Player(this.playerElementId, {
       width: '100%',
       height: '100%',
+      autoplay: 1,
       events: {
         onReady: (event) => this.onPlayerReady(event),
         onStateChange: (event) => this.onPlayerStateChange(event)
@@ -40,58 +45,135 @@ class YouTubePlayer {
         iv_load_policy: 3,
         rel: 0,
         enablejsapi: 1,
-        autoplay: 1,
-        loop: 0
+        loop: 0,
+        autoplay: 1
       }
     });
   }
 
   onPlayerReady(event) {
     this.playerReady = true;
-    event.target.playVideo();
-  }
-
-  onPlayerStateChange(event) {
-    console.log({ event, playerState: YT.PlayerState });
-
-    if (event.data == YT.PlayerState.PLAYING) {
+    if (this.channels.length > 0) {
+      const initialChannel = this.channels[0];
+      this.loadChannelVideo(initialChannel);
+      this.currentChannel = initialChannel;
+      event.target.playVideo();
+    } else {
+      console.warn('No channels available to load.');
     }
   }
 
-  stopVideo() {
-    this.player.stopVideo();
-    this.activateBuffering();
+  onPlayerStateChange(event) {
+    const stateNames = {
+      '-1': 'UNSTARTED',
+      0: 'ENDED',
+      1: 'PLAYING',
+      2: 'PAUSED',
+      3: 'BUFFERING',
+      5: 'CUED'
+    };
+
+    console.log(event);
+
+    console.log(`Current state: ${stateNames[event.data]}`);
+
+    const videoTitle = event.target.videoTitle;
+    if (videoTitle) this.videoTitle = videoTitle;
+
+    if (event.data === YT.PlayerState.PLAYING) {
+      this.isPlaying = true;
+
+      setTimeout(() => this.deactivateBuffering(), BUFFERING_TIMEOUT);
+      this.player.unMute();
+    } else if (event.data === YT.PlayerState.BUFFERING) {
+      this.activateBuffering();
+    } else if (event.data === YT.PlayerState.PAUSED) {
+      this.isPlaying = false;
+      this.activateBuffering();
+    } else if (event.data === YT.PlayerState.UNSTARTED) {
+      this.playVideo();
+    }
   }
 
   playVideo() {
-    this.player.playVideo();
-    this.deactivateBuffering();
+    if (
+      this.playerReady &&
+      this.player.getPlayerState() !== YT.PlayerState.PLAYING
+    ) {
+      console.log('Playing video:', this.videoTitle);
+      this.player.mute();
+      this.player.playVideo();
+    }
   }
 
-  activateBuffering = () => {};
-  deactivateBuffering = () => {};
+  pauseVideo() {
+    if (
+      this.playerReady &&
+      this.player.getPlayerState() === YT.PlayerState.PLAYING
+    ) {
+      this.player.pauseVideo();
+    }
+  }
+
+  changeChannel(channel) {
+    this.loadChannelVideo(channel);
+    this.currentChannel = channel;
+  }
+
+  changeChannelByOffset(offset) {
+    const currentIndex = this.channels.findIndex(
+      (channel) => channel.id === this.currentChannel.id
+    );
+    const newIndex =
+      (currentIndex + offset + this.channels.length) % this.channels.length;
+    this.changeChannel(this.channels[newIndex]);
+  }
+
+  nextChannel() {
+    this.changeChannelByOffset(1);
+  }
+
+  previousChannel() {
+    this.changeChannelByOffset(-1);
+  }
+
+  addBufferingClass = () => {
+    document.querySelector('#buffer-gif').classList.add('active');
+  };
+
+  removeBufferingClass = () => {
+    document.querySelector('#buffer-gif').classList.remove('active');
+  };
+
+  activateBuffering = () => {
+    if (!this.isBuffering) {
+      this.isBuffering = true;
+      this.addBufferingClass();
+      this.player.mute();
+    }
+  };
+
+  deactivateBuffering = () => {
+    this.isBuffering = false;
+    this.removeBufferingClass();
+    this.player.unMute();
+  };
 
   loadChannels() {
     fetch(this.channelsUrl)
       .then((res) => res.json())
       .then((res) => {
-        const channelsContainer = document.getElementById('channels');
-        res.channels.forEach((channel) => {
-          const button = document.createElement('button');
-          button.textContent = channel.type;
-          button.addEventListener('click', () =>
-            this.loadChannelVideo(channel)
-          );
-          channelsContainer.appendChild(button);
-        });
+        this.channels = res.channels;
       });
   }
 
   loadChannelVideo(channel) {
-    const videoId = channel.videos[0].url.split('=')[1];
+    const videoUrl = channel.videos[0].url;
+    const videoIdMatch = videoUrl.match(/(?:v=|\/)([0-9A-Za-z_-]{11})/);
+    const videoId = videoIdMatch ? videoIdMatch[1] : null;
     console.log('Loading video ID:', videoId);
 
-    if (this.playerReady && this.player.loadVideoById) {
+    if (videoId && this.playerReady) {
       this.player.loadVideoById(videoId);
     } else {
       console.error('Player not ready or invalid video ID:', videoId);
@@ -99,9 +181,27 @@ class YouTubePlayer {
   }
 
   addControlListeners() {
-    document.querySelector('#control-pause').addEventListener('click', () => {
+    document.querySelector('#control-power').addEventListener('click', () => {
       if (this.playerReady) {
-        this.stopVideo();
+        if (this.isPlaying) {
+          this.pauseVideo();
+        } else {
+          this.playVideo();
+        }
+      }
+    });
+
+    document.querySelector('#control-chup').addEventListener('click', () => {
+      this.nextChannel();
+    });
+
+    document.querySelector('#control-chdown').addEventListener('click', () => {
+      this.previousChannel();
+    });
+
+    document.querySelector('#control-mute').addEventListener('click', () => {
+      if (this.playerReady) {
+        this.player.isMuted() ? this.player.unMute() : this.player.mute();
       }
     });
   }
