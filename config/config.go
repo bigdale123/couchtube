487255bbaaacc9b28b242734959a7b36de987148
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2024-11-09 12:33:28 +0300

    Remove config

--- config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-var AllowedOrigins = map[string]bool{
-	"http://play.google.com":  true,
-	"http://youtube.com":      true,
-	"https://play.google.com": true,
-	"https://youtube.com":     true,
-}
