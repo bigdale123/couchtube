ebe8a25a965c5d786b37ddc3427bdec3bc7ffacf
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2024-11-09 15:37:12 +0300

    Improve readonly guard

--- middleware/readonly-guard.go
+++ middleware/readonly-guard.go
@@ -1,14 +1,21 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
+	"slices"
 )
 
 func ReadOnlyGuard(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte("Server is in read-only mode"))
+	restrictedHttpMethods := []string{http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch}
 
-		return
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if slices.Contains(restrictedHttpMethods, r.Method) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusServiceUnavailable)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Server is in read-only mode"})
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
 }
